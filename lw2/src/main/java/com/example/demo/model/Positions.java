ed9e02544949058431e30d58cbf235528f4b2368
YaroslavIonin <ioninyaroslav22@gmail.com>
2025-11-16 11:24:17 +0500

    update Positions

--- lw2/src/main/java/com/example/demo/model/Positions.java
+++ lw2/src/main/java/com/example/demo/model/Positions.java
@@ -4,13 +4,20 @@ import lombok.Getter;
 
 @Getter
 public enum Positions {
-    DEV(2.2),
-    HR(1.2),
-    TL(2.6);
+    DEV(2.2, false, "Разработчик"),
+    HR(1.7, false, "HR-специалист"),
+    QA(2.0, false, "Тестировщик"),
+    PO(2.5, true, "Владелец продукта"),
+    TPM(2.4, true, "Технический продакт менеджер"),
+    CTO(3.0, true, "Технический директор");
 
     private final double positionCoefficient;
+    private final boolean isManager;
+    private final String description;
 
-    Positions(double positionCoefficient){
+    Positions(double positionCoefficient, boolean isManager, String description){
         this.positionCoefficient = positionCoefficient;
+        this.isManager = isManager;
+        this.description = description;
     }
 }
