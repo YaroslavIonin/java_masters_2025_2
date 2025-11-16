08ff5b7cf792fde6a79f6326662c24f011b7ddd6
YaroslavIonin <ioninyaroslav22@gmail.com>
2025-11-16 11:24:00 +0500

    update calculate

--- lw2/src/main/java/com/example/demo/service/AnnualBonusServiceImpl.java
+++ lw2/src/main/java/com/example/demo/service/AnnualBonusServiceImpl.java
@@ -3,11 +3,18 @@ package com.example.demo.service;
 import com.example.demo.model.Positions;
 import org.springframework.stereotype.Service;
 
+import java.time.Year;
+
 @Service
 public class AnnualBonusServiceImpl implements AnnualBonusService {
 
     @Override
     public double calculate(Positions positions, double salary, double bonus, int workDays) {
-        return salary * bonus * 365 * positions.getPositionCoefficient() / workDays;
+        int daysInYear = getDaysInYear();
+        return salary * bonus * daysInYear * positions.getPositionCoefficient() / workDays;
+    }
+
+    private int getDaysInYear() {
+        return Year.now().length();
     }
 }
