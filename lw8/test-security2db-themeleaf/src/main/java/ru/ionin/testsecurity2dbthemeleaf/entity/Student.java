1a4831e5116fa7b6975bb39275201aa995368407
YaroslavIonin <ioninyaroslav22@gmail.com>
2025-12-24 14:38:45 +0500

    add entities

---
+++ lw8/test-security2db-themeleaf/src/main/java/ru/ionin/testsecurity2dbthemeleaf/entity/Student.java
@@ -0,0 +1,33 @@
+package ru.ionin.testsecurity2dbthemeleaf.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import jakarta.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "STUDENTS")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "faculty")
+    private String faculty;
+
+    @Column(name = "age")
+    private int age;
+}
