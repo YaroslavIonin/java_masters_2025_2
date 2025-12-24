5f1cfb61d5fc678b1b1dc406d0b4f30c6dc2c0d6
YaroslavIonin <ioninyaroslav22@gmail.com>
2025-12-24 11:22:05 +0500

    init proj

---
+++ lw8/test-security2db-themeleaf/src/test/java/ru/ionin/testsecurity2dbthemeleaf/TestSecurity2dbThemeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ionin.testsecurity2dbthemeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSecurity2dbThemeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
