22e1cfe45819c4973f0488e88718e524dc3b815e
YaroslavIonin <ioninyaroslav22@gmail.com>
2025-12-24 15:44:53 +0500

    add services

---
+++ lw8/test-security2db-themeleaf/src/main/java/ru/ionin/testsecurity2dbthemeleaf/service/CustomUserDetailsService.java
@@ -0,0 +1,38 @@
+package ru.ionin.testsecurity2dbthemeleaf.service;
+
+import org.jspecify.annotations.NonNull;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.ionin.testsecurity2dbthemeleaf.entity.User;
+import ru.ionin.testsecurity2dbthemeleaf.repository.UserRepository;
+
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(@NonNull String usernameOrEmail) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(usernameOrEmail);
+        if (user != null) {
+            return new org.springframework.security.core.userdetails.User(
+                    user.getEmail(),
+                    user.getPassword(),
+                    user.getRoles().stream()
+                            .map((role) -> new SimpleGrantedAuthority(role.getName()))
+                            .collect(Collectors.toList()));
+        } else {
+            throw new UsernameNotFoundException("Invalid email or password");
+        }
+    }
+}
---
+++ lw8/test-security2db-themeleaf/src/main/java/ru/ionin/testsecurity2dbthemeleaf/service/UserService.java
@@ -0,0 +1,14 @@
+package ru.ionin.testsecurity2dbthemeleaf.service;
+
+import ru.ionin.testsecurity2dbthemeleaf.dto.UserDto;
+import ru.ionin.testsecurity2dbthemeleaf.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+    void saveUser(UserDto userDto);
+
+    User findUserByEmail(String email);
+
+    List<UserDto> findAllUsers();
+}
